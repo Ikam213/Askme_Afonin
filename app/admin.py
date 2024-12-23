e91b062b56d47947ca015bb9b6bca90222911e47
Ikam213 <afonin05@yandex.ru>
2024-12-24 02:49:17 +0300

    changed localization, fixed and improved hw3, made registration and logging in for hw4

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,7 @@ from app import models
 
 # Register your models here.
 admin.site.register(models.Question)
-admin.site.register(models.Person)
+admin.site.register(models.Profile)
 admin.site.register(models.Like)
 admin.site.register(models.Tag)
 admin.site.register(models.Answer)
