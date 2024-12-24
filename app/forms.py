e8f8791d66a99232d0ebae9f2bd8f46f5f239959
Ikam213 <afonin05@yandex.ru>
2024-12-24 13:40:39 +0300

    added saving of questions and answers, redirection and other fixes

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-from .models import Answer
+from .models import Answer, Question
 
 class LoginForm(forms.Form):
     username = forms.CharField(label='Логин')
@@ -14,8 +14,8 @@ class LoginForm(forms.Form):
 class RegisterForm(forms.ModelForm):
     login = forms.CharField(label='Логин', error_messages={'required': 'Это поле обязательно'})
     email = forms.EmailField(label='Адрес электронной почты', error_messages={'required': 'Это поле обязательно'})
-    password = forms.CharField(widget=forms.PasswordInput, label='Пароль', error_messages={'required': 'Это поле обязательно'})
-    password_confirmation = forms.CharField(widget=forms.PasswordInput, label='Подтверждение пароля', error_messages={'required': 'Это поле обязательно'})    
+    password = forms.CharField(widget=forms.PasswordInput, label='Пароль', error_messages={'required': 'Это поле обязательно', 'min_length': 'Пароль должен содержать не менее 8 символов', 'max_length': 'Пароль слишком длинный'}, min_length=8, max_length=20)
+    password_confirmation = forms.CharField(widget=forms.PasswordInput, label='Подтверждение пароля', error_messages={'required': 'Это поле обязательно', 'min_length': 'Пароль должен содержать не менее 8 символов', 'max_length': 'Пароль слишком длинный'}, min_length=8, max_length=20)    
 
     class Meta:
         model = User
@@ -46,3 +46,12 @@ class AnswerForm(forms.ModelForm):
     class Meta:
         model = Answer
         fields = ('body',)    
+
+class QuestionForm(forms.ModelForm):
+    title = forms.CharField(label='Тема вопроса', error_messages={'required': 'Это поле обязательно', 'max_length': 'Тема слишком длинная'}, max_length=50)
+    body = forms.CharField(label='Текст вопроса', error_messages={'required': 'Это поле обязательно'}, widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Введите ваш вопрос'}))
+    tags = forms.CharField(label='Тэги', error_messages={'required': 'Это поле обязательно'}, widget=forms.Textarea(attrs={'class': 'text-area', 'rows': 1,'placeholder': 'Введите ваши тэги'}))
+
+    class Meta:
+        model = Question
+        fields = ('title', 'body', 'tags')
--- app/views.py
+++ app/views.py
@@ -6,50 +6,72 @@ from django.contrib import auth
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
-from app.forms import LoginForm, RegisterForm, AnswerForm
+from app.forms import LoginForm, RegisterForm, AnswerForm, QuestionForm
+from django.shortcuts import resolve_url
 
-all_tags: Final = Tag.objects.distinct().order_by('tag_title')
+all_tags: Final = Tag.objects.distinct().order_by('?')[:5]
+top_members: Final = Profile.objects.distinct().order_by('?')[:5]
 
 def index(request):
     questions = Question.objects.all()
     page, page_count = paginate(questions, request, 5)
-    return render(request, 'index.html', {'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags })
+    return render(request, 'index.html', {'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags, 'top_members': top_members})
 
 def hot(request):
     hot_questions = Question.objects.order_by('-created_at')
     page, page_count = paginate(hot_questions, request, 5)
-    return render(request, 'hot.html', {'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags  })
+    return render(request, 'hot.html', {'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags, 'top_members': top_members})
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    next_url = request.GET.get('next', reverse('index'))
     form = RegisterForm()
     if request.method == 'POST':
         form = RegisterForm(request.POST)
+        next_url = request.POST.get('next', reverse('index'))
         if form.is_valid():
             form.save()
-            return redirect(reverse('lo'))
-    return render(request, 'signup.html', {'all_tags': all_tags, 'form': form})
+            return redirect(next_url)
+    return render(request, 'signup.html', {'all_tags': all_tags, 'form': form, 'next': next_url, 'top_members': top_members})
 
 def login(request):
-    form = LoginForm
+    next_url = request.GET.get('next', reverse('index'))
+    form = LoginForm()
     if request.method == 'POST':
         form = LoginForm(request.POST)
+        next_url = request.POST.get('next', reverse('index'))
         if form.is_valid():
             user = auth.authenticate(request, **form.cleaned_data)
             if user:
                 auth.login(request, user)
-                return redirect(reverse('index'))
+                return redirect(next_url)
             form.add_error('login', 'Неправильный логин или пароль')
-    return render(request, 'login.html', {'all_tags': all_tags, 'form': form})
+    return render(request, 'login.html', {'all_tags': all_tags, 'form': form, 'next': next_url, 'top_members': top_members})
 
 def tags(request, tag_title):
     tag = get_object_or_404(Tag, tag_title = tag_title)
     tag_questions = Question.objects.filter(tags=tag)
     page, page_count = paginate(tag_questions, request, per_page=5)
-    return render(request, 'tags.html', {'current_tag_name': tag_title, 'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags  })
+    return render(request, 'tags.html', {'current_tag_name': tag_title, 'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags, 'top_members': top_members})
 
-@login_required
+@login_required(login_url='login')
 def ask(request):
-    return render(request, 'ask.html', {'all_tags': all_tags})
+    form = QuestionForm()
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            user_profile = Profile.objects.get(user=request.user)
+            new_question = form.save(commit=False)
+            new_question.user = user_profile
+            new_question.save()
+            tags = form.cleaned_data['tags'].split(',')
+            for tag_title in tags:
+                tag, created = Tag.objects.get_or_create(tag_title=tag_title.strip())
+                new_question.tags.add(tag)
+            new_question.save()            
+            return redirect('question', question_id=new_question.id)
+    return render(request, 'ask.html', {'all_tags': all_tags, 'form': form, 'top_members': top_members})
 
 def question(request, question_id):
     one_question = get_object_or_404(Question, id=question_id)
@@ -64,12 +86,12 @@ def question(request, question_id):
             new_answer.user = user_profile
             new_answer.question = one_question
             new_answer.save()
-            return redirect(reverse('question', args=[question_id]))
-    return render(request, 'question.html', {'question': one_question, 'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags, 'form': form  })
+            return redirect(f"{request.path}#answer-{new_answer.id}")
+    return render(request, 'question.html', {'question': one_question, 'page': page, 'page_count': page_count, 'page_number': page.number, 'all_tags': all_tags, 'form': form, 'top_members': top_members})
 
 @login_required
 def settings(request):
-    return render(request, 'settings.html', {'all_tags': all_tags})
+    return render(request, 'settings.html', {'all_tags': all_tags, 'top_members': top_members})
 
 def paginate(object_list, request, per_page):
     paginator = Paginator(object_list, per_page)
